eaf5ff20cd82650669f3d83d0a0a1d315f46ac8b
dzakimutammadien <94619488+dzakimutammadien@users.noreply.github.com>
2025-04-24 23:05:35 +0700

    Add dummy content for Home, Saved, About, and Detail screens

---
+++ app/(tabs)/about.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function About() {
+  return (
+    <View style={{ padding: 20 }}>
+      <Text>Ini Halaman Tentang Aplikasi</Text>
+    </View>
+  );
+}
---
+++ app/(tabs)/saved.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function Saved() {
+  return (
+    <View style={{ padding: 20 }}>
+      <Text>Ini Halaman Berita Tersimpan</Text>
+    </View>
+  );
+}
