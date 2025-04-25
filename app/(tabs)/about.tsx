3eedc657c5840c97c49409ba689ce71632da5c9b
dzakimutammadien <94619488+dzakimutammadien@users.noreply.github.com>
2025-04-25 23:14:31 +0700

    Reset Ulang

--- app/(tabs)/about.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function About() {
-  return (
-    <View style={{ padding: 20 }}>
-      <Text>Ini Halaman Tentang Aplikasi</Text>
-    </View>
-  );
-}
